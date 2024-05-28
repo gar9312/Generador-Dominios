2a9b9fe9588e3863f556f1e2624db67ac4118c9e
Ursula Jardon <155091638+gar9312@users.noreply.github.com>
2024-05-28 03:22:24 +0000

    Finish

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronoun = ["el", "nuestro"];
+  let adj = ["hermoso", "grande", "rápido"];
+  let noun = ["leon", "ciervo", "avión"];
+
+  for (let pron of pronoun) {
+    for (let ad of adj) {
+      for (let no of noun) {
+        console.log(`${pron}${ad}${no}.com`);
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
 };
